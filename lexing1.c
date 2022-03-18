4e2541ad05365da97340ffd13cc8825953a2a135
oessayeg <offissayegh001@gmail.com>
2022-03-18 13:00:26 +0100

    test

---
+++ lexing1.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexing1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oessayeg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 12:19:59 by oessayeg          #+#    #+#             */
+/*   Updated: 2022/03/18 12:50:14 by oessayeg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+char **split_first_input(char *input)
+{
+	int		i;
+	char	**return_value;
+
+	i = 0;
+	while (input[i] != '\0')
+	{
+		if (input[i] == '|')
+			push_before_pipe(input, i, return_value);
+		i++;
+	}
+}
+
+int	is_del(char *input, int index)
+{
+	if (input[i] == '|' || input[i] == '>')
+		return (1);
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oessayeg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 12:13:11 by oessayeg          #+#    #+#             */
+/*   Updated: 2022/03/18 12:30:43 by oessayeg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+#endif
