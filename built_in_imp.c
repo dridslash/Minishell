066ca64ff6837a78384e28186559b25284684feb
Mohamed khalil Naqqad <mnaqqad@e1r7p5.1337.ma>
2022-03-18 20:39:26 +0100

    changes

--- built_in_imp.c
+++ built_in_imp.c
@@ -6,7 +6,7 @@
 /*   By: mnaqqad <mnaqqad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 15:22:42 by mnaqqad           #+#    #+#             */
-/*   Updated: 2022/03/18 15:16:24 by mnaqqad          ###   ########.fr       */
+/*   Updated: 2022/03/18 19:23:01 by mnaqqad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,11 @@ void ft_cd(int argc, char **argv)
      else
      chdir(get_home);
      }
-     printf("%s\n",getcwd(current_dir,FILE_N_MAX));
+     else
+     {
+         if(chdir(argv[index_tmp]) != 0)
+         perror("my shell : cd ");
+     }
 }
 
 t_env *ft_env(char **envi)
@@ -89,31 +93,107 @@ int get_equal_index(char *table)
     return (-1);
 }
 
+void append_env_vr(t_env **envv,char *var)
+{
+    t_env *tmp;
+    tmp = (*envv);
+    char **holder_first;
+    while(tmp)
+    {
+        holder_first = ft_split(tmp->path_env,'=');
+        if(ft_strcmp(holder_first[0],var) == 0)
+        {
+            ft_strjoin_non_free(tmp->path_env,var);
+            break;
+        }
+        tmp = tmp->next_env;
+    }
+}
+
+char *get_name_of_env_var(char *tb)
+{
+    int  i =0;
+    int index_tmp = 2;
+    char *chr = ft_strdup(tb);
+    while(chr[i])
+    {
+        if(chr[i] == '=')
+        {
+        chr[i] = '\0';
+        break;
+        }
+        i++;
+    }
+    return (chr);
+}
+
+void part_one_of_export(t_env **envv,char **argv,int index_tmp)
+{
+    int i =0;
+    int locker = 0;
+    t_env *aff = (*envv);
+    char *holder = argv[index_tmp + 1];
+    if(argv[index_tmp + 1] != NULL && get_equal_index(argv[index_tmp + 1]) != -1)
+         {
+             while(aff)
+             {
+                 if(ft_strcmp(get_name_of_env_var(aff->path_env),get_name_of_env_var(holder)) == 0)
+                 {
+                     aff->path_env = holder;
+                     locker ++;
+                     break;
+                 }
+                 aff = aff->next_env;
+             }
+            if(locker == 0)
+                     {
+                     create_env(envv,holder);
+                     }
+                     aff = (*envv);
+             while(aff)
+                {
+                 printf("%s\n",aff->path_env);
+                 aff = aff->next_env;
+                } 
+         }
+         else
+         {
+             printf("insert an equal or go away man !!");
+         }
+         
+}
+
 void ft_export_var(t_env **envv,char **argv)
 {
     int i =0;
-    int pattern_counter = 0;
+    int equal_sing_her = 0;
     int index_tmp = 1;
-    t_env *tmp = (*envv);
-    t_env *aff = (*envv);
+    char *holder = argv[index_tmp + 1];
     if(ft_strcmp(argv[index_tmp],"export") == 0)
      {
-         if(argv[index_tmp + 1] != NULL)
+         while(holder[i])
          {
-             //if (ft_strnstr())
+             if(holder[i] == '+' && holder[i + 1] != '=')
+             {
+             printf("really + without an = go out !! \n");
+               break;  
+             }
+             if (/*holder[i] == '-' ||*/ (holder[i] == '-' && holder[i + 1] == '='))
+             {
+                printf("minus sign no no !! \n");
+               break;  
+             }
+             i++;
          }
+         part_one_of_export(envv,argv,index_tmp);
      }
-    while(aff)
-    {
-        printf("%s\n",aff->path_env);
-        aff = aff->next_env;
-    }
     
 }
 
 int main(int argc,char **argv, char **env)
 {
     t_env *envv = ft_env(env);
-    ft_export_var(&envv,argv);
+   ft_export_var(&envv,argv);
+    // ft_cd(argc , argv);
     return 0;
 }
