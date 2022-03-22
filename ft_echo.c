49794b8ee14e578bf595afe366e6901b4d5a9daa
Mohamed khalil Naqqad <mnaqqad@e1r7p5.1337.ma>
2022-03-22 12:04:51 +0100

    echo 100%_march_22

--- ft_echo.c
+++ ft_echo.c
@@ -6,41 +6,74 @@
 /*   By: mnaqqad <mnaqqad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 14:09:45 by mnaqqad           #+#    #+#             */
-/*   Updated: 2022/03/22 10:15:45 by mnaqqad          ###   ########.fr       */
+/*   Updated: 2022/03/22 12:03:52 by mnaqqad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 
-int charachter_eater(int argc , char **argv, int is_there_option)
+int charachter_eater(int argc , char ** argv,int is_there_stopper_newline, int start)
 {
-    int i = 2;
-    int count_is_there = 0;
+    int i = 0;
+    i = start;
+    if(is_there_stopper_newline == 1)
+    i++;
+    int count_if_there_an_arg = 0;
     while(argv[i])
     {
         if(argv[i] != NULL)
         {
-            count_is_there ++;
+            count_if_there_an_arg++;
         }
+        i++;
     }
-    return (count_is_there);
+    return (count_if_there_an_arg);
 }
 
 void ft_echo(int argc, char **argv)
 {
     int index_tmp = 1;
     int option_of_echo = index_tmp + 1;
-    if(ft_strcmp(argv[index_tmp],"echo") == 0)
+    int args = 0;
+    if(argv[index_tmp] != NULL && (ft_strcmp(argv[index_tmp], "echo") == 0))
     {
-        if(argv[option_of_echo] == NULL)
+        if(argv[option_of_echo] != NULL)
+        {
+        if(ft_strcmp(argv[option_of_echo],"-n") != 0)
+        {
+           if(charachter_eater(argc,argv,0,option_of_echo) > 0)
+           {
+               args = option_of_echo;
+               while(argv[args])
+               {
+                   write(1,argv[args],ft_strlen(argv[args]));
+                   args++;
+               }
+               write(1,"\n",1);
+           }
+        }
+        else
+        {
+           if(charachter_eater(argc,argv,1,option_of_echo) > 0)
+           {
+               args = option_of_echo + 1;
+               while(argv[args])
+               {
+                   write(1,argv[args],ft_strlen(argv[args]));
+                   args++;
+               }
+           }
+           else
+           write(1,"",1);
+        }
+        }
+        else
         {
             write(1,"\n",1);
         }
-        else if (argv[option_of_echo] != NULL && (ft_strcmp(argv[option_of_echo],"-n") == 0))
-        {
-            write(1,"",1);
-        }
     }
+    else
+    printf("there is no echo\n");
 }
 int main(int argc, char **argv)
 {
